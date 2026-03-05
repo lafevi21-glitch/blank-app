d49b749ffeddf84ebae935c72307a94c25372410
lafevi21-glitch <lafevi21@gmail.com>
2026-03-05 21:11:05 +0100

    Add files via upload

--- pages/multiapps.py
+++ pages/multiapps.py
@@ -30,7 +30,19 @@ redirected_user = app.session_state.get("logged_user", "Guest")
 def multiapps_main(username):
     
     app.header("✨ Multiapps V" + ver)
-    app.subheader("Welcome"+ username + ".")
+    app.subheader("Welcome "+ username + ".")
     app.write("This website aimed to serve me, Mateo. You'll be able to find some quite useful apps such as a music player and a calculator")
+    app.divider()
+    
+    # Music Player.
+
+    app.subheader("Music Library 🎹") 
+    app.write("Welcome my music library!")
+    url = app.text_input("Input the URL of your music.")
+    try:
+        if app.button("Play"):
+            app.audio(url, format="audio/mp3")
+    except Exception:
+        app.error("Invalid URL provided, **make sure it's MP3**.")
 
 multiapps_main(redirected_user)
