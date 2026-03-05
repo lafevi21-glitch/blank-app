58518f10e68ee4765f93fab54cf1de435671a1cb
lafevi21-glitch <lafevi21@gmail.com>
2026-03-05 17:48:29 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,13 @@
 import streamlit as app
+from multiapps import redirected_user
+
+multiapps = app.Page("multiapps.py")
 
 matt = {"username": "Mateo", "password": "210812"}
 ver = "1"
 
-def multiapps_main(username):
-    
-    app.header("✨ Multiapps V" + ver)
-    app.subheader("Welcome"+ username +".")
-    app.write("This website aimed to serve me, Mateo. You'll be able to find some quite useful apps such as a music player and a calculator")
-
 def authenticate():
-    app.title("Authenticate.")
+    app.header("Authenticate.")
     app.write("Please enter the password and agree to the terms and conditions to be able to proceed to the app.")
     
     na_username = app.text_input("Enter username")
@@ -19,7 +16,9 @@ def authenticate():
     if app.button("Login"):
         if na_username == matt["username"] and na_password == matt["password"]:
             app.empty()
-            multiapps_main(na_username)
+            redirected_user = na_username
+            app.navigation(multiapps)
+
 
 
 authenticate()
