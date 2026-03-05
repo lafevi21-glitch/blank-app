626ec561eb6c091998980af184bf32985eb384cf
lafevi21-glitch <lafevi21@gmail.com>
2026-03-05 19:59:47 +0100

    Delete pages/multiapps.py

--- pages/multiapps.py
+++
@@ -1,9 +0,0 @@
-import streamlit as app
-
-redirected_user = none
-
-def multiapps_main(username):
-    
-    app.header("✨ Multiapps V" + ver)
-    app.subheader("Welcome"+ username + ".")
-    app.write("This website aimed to serve me, Mateo. You'll be able to find some quite useful apps such as a music player and a calculator")
