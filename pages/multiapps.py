624a2bd67984672b3fd3f4524df61d18e902196e
lafevi21-glitch <lafevi21@gmail.com>
2026-03-05 21:48:37 +0100

    Add files via upload

--- pages/multiapps.py
+++ pages/multiapps.py
@@ -1,4 +1,9 @@
 import streamlit as app
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseUpload
+import io
+
 
 if not "logged_user" in app.session_state:
     app.error("Please, make sure you are logged!")
@@ -37,12 +42,47 @@ def multiapps_main(username):
     # Music Player.
 
     app.subheader("Music Library 🎹") 
-    app.write("Welcome my music library!")
+    app.write("Welcome to my music library!")
     url = app.text_input("Input the URL of your music.")
     try:
         if app.button("Play"):
             app.video(url, format="audio/mp3")
     except Exception:
         app.error("Invalid URL provided, **make sure it's MP3**.")
+    
+    # Music Player.
+
+    app.divider()
+    
+    # Cloud Service.
+
+    app.subheader("Cloud ☁")
+
+    app.write("In this section you're able to upload files directly on Google Drive!")
+
+    def get_drive_service():
+        # This pulls directly from the secrets we just configured
+        info = st.secrets["gcp_service_account"]
+        creds = service_account.Credentials.from_service_account_info(info)
+        return build('drive', 'v3', credentials=creds)
+
+    def upload_file(uploaded_file, folder_id):
+        service = get_drive_service()
+        file_metadata = {'name': uploaded_file.name, 'parents': [folder_id]}
+        media = MediaIoBaseUpload(io.BytesIO(uploaded_file.getvalue()), 
+                              mimetype=uploaded_file.type)
+    
+        file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+        return file.get('id')
+
+    st.title("Secure Drive Upload")
+
+    
+    FOLDER_ID = "PASTE_YOUR_FOLDER_ID_HERE"
+
+    myfile = st.file_uploader("Choose a file")
+    if myfile and st.button("Upload to Drive"):
+        fid = upload_file(myfile, FOLDER_ID)
+        st.success(f"Uploaded! File ID: {fid}")
 
 multiapps_main(redirected_user)
