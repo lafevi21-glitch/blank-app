162428be6cf34c2b4e1555de013159b717fa4841
lafevi21-glitch <lafevi21@gmail.com>
2026-03-05 17:53:41 +0100

    Add files via upload

--- multiapps.py
+++ multiapps.py
@@ -1,9 +1,11 @@
 import streamlit as app
 
-redirected_user = none
+redirected_user = None
 
 def multiapps_main(username):
     
     app.header("✨ Multiapps V" + ver)
     app.subheader("Welcome"+ username + ".")
     app.write("This website aimed to serve me, Mateo. You'll be able to find some quite useful apps such as a music player and a calculator")
+
+multiapps_main(redirected_user)
